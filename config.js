e4cff8c84c7d5fd03e789b46888372831cfd1399
NGO SANG <ngosangns@gmail.com>
2021-12-20 08:58:31 +0700

    add readme.md

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 let download_path = "./4_ky2_2021_2022.xlsx";
 let download_name = "4_ky2_2021_2022.xlsx";
+let title = "Kì 2 Năm học 2021 - 2022";
+
+// ---
 
 let download_link = document.getElementById("download-link");
 download_link.setAttribute("href", download_path);
 download_link.setAttribute("download", download_name);
-
 let school_year = document.getElementById('school-year');
-school_year.innerHTML = "Kì 2 Năm học 2021 - 2022";
+school_year.innerHTML = title;
